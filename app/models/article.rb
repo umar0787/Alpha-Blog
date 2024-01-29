a6898661ad796398a2434646d5d352d58c4f7498
umar0787 <umariiqbal0787@gmail.com>
2024-01-29 05:34:26 +0500

    Add create article functionality with the validation and flash message

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-  validates :title , presence: true , length: {minimum:6 , maximum:10}
+  validates :title , presence: true , length: {minimum:3 , maximum:15}
   validates :description , presence: true , length: {minimum:10 , maximum:300}
 end
