c72a073bb6024327f8b992a342d969653087e6dc
umarIqbal <umariiqbal0787@gmail.com>
2024-04-03 07:01:36 +0500

    Add admin user access

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
 
     def destroy
         @user.destroy
-        session[:user_id] = nil
+        session[:user_id] = nil if @user == current_user
         flash[:notice] = "User and all the associated articles were deleted."
         redirect_to articles_path
     end
@@ -56,7 +56,7 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-        if current_user != @user
+        if current_user != @user && !current_user.admin?
           flash[:alert] = "You can edit or delete your own article"
           redirect_to @user
         end
