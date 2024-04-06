5d4ba09c0f2edbd5742d0eb1966d33c7b4ecb370
umarIqbal <umariiqbal0787@gmail.com>
2024-04-06 10:23:16 +0500

    Add many to many association from backend

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   belongs_to :user
   validates :title , presence: true , length: {minimum:3 , maximum:15}
   validates :description , presence: true , length: {minimum:10 , maximum:300}
